86537d373684e923ff50a0033f2e421662402ff5
otservme <root@remote.otpanel.com>
2014-09-01 04:02:48 +0000

    first commit

---
+++ pages/serverinfo.php
@@ -0,0 +1,140 @@
+<?php
+if(!defined('INITIALIZED'))
+	exit;
+	$main_content .= '<br><center>
+		<table border="0" cellpadding="4" cellspacing="1" width="95%">
+			<tr bgcolor="'.$config['site']['vdarkborder'].'">
+				<td colspan="2"><font class="white"><b>Status</b></font></td>
+			</tr>
+			<tr bgcolor="'.$config['site']['vdarkborder'].'">
+				<td width="50%"><font class="white"><b>Name</b></font></td><td><font class="white"><b>Value</b></font></td>
+			</tr>
+			<tr bgcolor="'.$config['site']['darkborder'].'">
+				<td>Server Status:</td><td>'.(($config['status']['serverStatus_online'] == 1) ? '<b>Online</b>' : '<b>Offline</b>').'</td>
+			</tr>
+		</table>
+		<br>';
+	$main_content .= '<table border="0" cellpadding="4" cellspacing="1" width="95%">
+			<tr bgcolor="'.$config['site']['vdarkborder'].'">
+				<td colspan="2"><font class="white"><b>Rates</b></font></td>
+			</tr>
+			<tr bgcolor="'.$config['site']['vdarkborder'].'">
+				<td><font class="white"><b>Name</b></font></td><td><font class="white"><b>Value</b></font></td>
+			</tr>
+			<tr bgcolor="'.$config['site']['darkborder'].'">
+				<td width="50%">Experience</td><td>'.$rateExperience.'</td>
+			</tr>
+			<tr bgcolor="'.$config['site']['lightborder'].'">
+				<td>Skill</td><td>'.$config['server']['rateSkill'].'x</td>
+			</tr>
+			<tr bgcolor="'.$config['site']['darkborder'].'">
+				<td>Magic</td><td>'.$config['server']['rateMagic'].'x</td>
+			</tr>
+			<tr bgcolor="'.$config['site']['lightborder'].'">
+				<td>Loot</td><td>'.$config['server']['rateLoot'].'x</td>
+			</tr>
+		</table><br>';
+
+	$main_content .= '<table border="0" cellpadding="4" cellspacing="1" width="95%">
+		<tr bgcolor="'.$config['site']['vdarkborder'].'">
+			<td colspan="2"><font class="white"><b>Info Server</b></font></td>
+		</tr>
+		<tr bgcolor="'.$config['site']['vdarkborder'].'">
+			<td width="50%"><font class="white"><b>Name</b></font></td><td><font class="white"><b>Value</b></font></td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>World Type</td><td>';
+			$main_content .='</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Server motd</td><td>'.$config['server']['motd'].'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Last joined</td><td><a href="?subtopic=characters&name='.urlencode($query2['name']).'">'.$query2['name'].'</a></td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Best Level</td><td><a href="index.php?subtopic=characters&name='.urlencode($query['name']).'">'.$query['name'].'</a> (<b>Level&nbsp;'.$query['level'].'</b>)</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Free Houses</td><td>'.$housesfree[0].'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Rented Houses:</td><td>'.$housesrented[0].'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Banned accounts:</td><td>'.$banned[0].'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Accounts in database:</td><td>'.$accounts[0].'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Players in database:</td><td>'.$players[0].'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Guilds in databese:</td><td>'.$guilds[0].'</td>
+		</tr>
+	</table><br>';
+	$main_content .= '<table border="0" cellpadding="4" cellspacing="1" width="95%">
+		<tr bgcolor="'.$config['site']['vdarkborder'].'">
+			<td colspan="2"><font class="white"><b>Frags</b></font></td>
+		</tr>
+		<tr bgcolor="'.$config['site']['vdarkborder'].'">
+			<td width="50%"><font class="white"><b>Name</b></font></td><td><font class="white"><b>Value</b></font></td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>White Skull Time</td><td>'.$whiteSkullTime.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Red Skull Time</td><td>'.$redSkullLength.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Black Skull Time</td><td>'.$blackSkull.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Frags to Red Skull</td><td>'.$fragsToRedSkull.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Frags to Black Skull</td><td>'.$fragsToBlackSkull.'</td>
+		</tr>
+	</table><br>';
+	$main_content .= '<table border="0" cellpadding="4" cellspacing="1" width="95%">
+		<tr bgcolor="'.$config['site']['vdarkborder'].'">
+			<td colspan="2"><font class="white"><b>Onther information</b></font></td>
+		</tr>
+		<tr bgcolor="'.$config['site']['vdarkborder'].'">
+			<td width="50%"><font class="white"><b>Name</b></font></td><td><font class="white"><b>Value</b></font></td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Premium</td><td>'.$freePremium.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Bank System</td><td>'.$bankSystem.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Guild halls</td><td>'.$guildHalls.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Kick Time</td><td>'.$idleKickTime.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>PZ Lock</td><td>'.$pzLocked.'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Protection Level</td><td>'.$config['server']['protectionLevel'].'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td>Level to buy house</td><td>'.$config['server']['levelToBuyHouse'].'</td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td>Level to create guild</td><td>'.$config['site']['guild_need_level'].'</td>
+		</tr>
+		<!--
+		<tr bgcolor="'.$config['site']['darkborder'].'">
+			<td></td><td></td>
+		</tr>
+		<tr bgcolor="'.$config['site']['lightborder'].'">
+			<td></td><td></td>
+		</tr>
+		-->
+	</table><br>';
+	$main_content .= '</center>';
---
+++ pages/shopadmin.php
@@ -0,0 +1,84 @@
+﻿<?php
+if(!defined('INITIALIZED'))
+        exit;
+ 
+$SQL = $GLOBALS['SQL'];
+       
+if($group_id_of_acc_logged >= $config['site']['access_admin_panel'])
+{
+        //$main_content .= 'I recommend you to add items and containers by this script: <a href="http://otland.net/f479/gesior2012-items-shop-installation-administration-170654/" target="_blank">http://otland.net/f479/gesior2012-items-shop-installation-administration-170654/</a><br />';
+        //$main_content .= 'Script to add points etc. to shop on www is not available. I also recommend to <b>not download from forum/use old script</b> as it is bugged!';
+       
+        //validar os dados
+        if($action == "shop_save") {
+                $points                         = trim($_POST['points']);
+                $itemid1                        = trim($_POST['itemid1']);
+                $count1                         = trim($_POST['count1']);
+                $itemid2                        = trim($_POST['itemid2']);
+                $count2                         = trim($_POST['count2']);
+                $offer_type                     = trim($_POST['offer_type']);
+                $offer_description      = trim($_POST['offer_description']);
+                $offer_name                     = trim($_POST['offer_name']);
+               
+                if(empty($points) && empty($itemid1) && empty($offer_name)) {
+                        $main_content .= '<strong>Você deve preencher pelo menos os pontos, id item 1 e o nome da oferta!</strong><br /><br /><a href="?subtopic=shopadmin">Voltar</a>';
+                } else {
+                        $sql_save = sprintf(
+                                "INSERT INTO `z_shop_offer` (points,itemid1,count1,itemid2,count2,offer_type,offer_description,offer_name)VALUES('%s','%s','%s','%s','%s','%s','%s','%s')",
+                                (empty($points)  ? 0 : $points),
+                                (empty($itemid1) ? 0 : $itemid1),
+                                (empty($count1)  ? 0 : $count1),
+                                (empty($itemid2) ? 0 : $itemid2),
+                                (empty($count2)  ? 0 : $count2),
+                                $offer_type,
+                                $offer_description,
+                                $offer_name
+                        );
+                        $SQL->query($sql_save);
+                        $main_content .= '<strong>Oferta salva com sucesso!</strong><br /><br /><a href="?subtopic=shopadmin">Voltar</a>';
+                }
+               
+        } else {
+                $main_content .= <<<EOD
+                        <style type="text/css">
+                        h1.admshop{margin:0;padding:0;}
+                        label.admshop{float:left;width:100px;}
+                        div.clear{clear:both;}
+                        p.border{border-bottom:1px solid #D4C0A1;padding:3px;}
+                        form input, form select, form button, form reset{padding:3px;}
+                        input.bt{padding:3px 20px;cursor:pointer;}
+                        </style>
+                        <h1 class="admshop"><strong>Bem vindo ao Administrador do Shop!</strong></h1>
+                        <hr />
+                        <form method="post" action="?subtopic=shopadmin&action=shop_save">
+                                <p class="border"><strong>Nome / Descrição da oferta</strong></p>
+                                <p><label class="admshop">Oferta: </label><input type="text" name="offer_name" size="50" maxlength="100" /></p>
+                                <p><label class="admshop">Descrição: </label><input type="text" name="offer_description" size="50" maxlength="1000" /></p>
+                                <p><label class="admshop">Qtde. pontos: </label><input type="text" name="points" size="5" maxlength="9" /></p>
+                                <p class="border"><strong>Tipo da oferta</strong></p>
+                                <p><label class="admshop">Tipo: </label><select name="offer_type"><option value="item" selected="selected">Item</option><option value="container">Container</option></select></p>
+                                <p class="border"><strong>Configuração do item 1</strong></p>
+                                <p><label class="admshop">ID Item 1: </label><input type="text" name="itemid1" size="10" /></p>
+                                <p><label class="admshop">Qtde. Item 1: </label><input type="text" name="count1" size="10" /></p>
+                                <p class="border"><strong>Configuração do item 2</strong></p>
+                                <p><label class="admshop">ID Item 2: </label><input type="text" name="itemid2" size="10" /></p>
+                                <p><label class="admshop">Qtde. Item 2: </label><input type="text" name="count2" size="10" /></p>
+                                <p class="border"><br /></p>
+                                <input type="submit" value="Salvar" class="bt" />
+                        </form>
+                        <div class="clear"></div>
+EOD;
+//não mude a linha linha de cima (EOD), deixe ela encostada no cantinho se não vai dar erro no PHP !!!
+        }//fim validação
+ 
+}
+else
+{
+        $main_content .= 'Sorry, you have not the rights to access this page.';
+}
+ 
+/******************************************************************
+* SYSTEMA DE ADMINISTRAÇÃO ONLINE DO WEBSHOP GESIOR 2012 BY DEZON *
+*    TODOS OS DIREITOS, POR FAVOR, NÃO REMOVER ESSES CRÉDITOS     *
+*       FEITO EXCLUSIVAMENTE PARA O SITE WWW.TIBIAKING.COM        *
+******************************************************************/
