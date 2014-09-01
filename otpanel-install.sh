2671b6e2e4036dff3b18a8df8d27b3846e7ea71e
otservme <root@remote.otpanel.com>
2014-09-01 04:35:01 +0000

    correcao

--- otpanel-install.sh
+++ otpanel-install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 wget https://github.com/otservme/gesior-tfs10/archive/master.zip
-rm -r /home/otserv/www;
+rm -rf /home/otserv/www/*;
 unzip master.zip -d /home/otserv/;
-mv /home/otserv/gesior-tfs10-master /home/otserv/www;
+cp -rf /home/otserv/gesior-tfs10-master/* /home/otserv/www;
 chown -R www-data.www-data /home/otserv;
 echo "Pronto, instalado com sucesso!"
-
